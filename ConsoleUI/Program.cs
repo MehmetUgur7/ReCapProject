a24c0368ae240aa0d37e71ace027ab2d9dadf660
MehmetUgur7 <77232030+MehmetUgur7@users.noreply.github.com>
2021-02-10 11:17:22 +0300

    30 Ocak Ödevi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
             CarManager carManager = new CarManager(new InMemoryCarDal());
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.BrandName);
+                Console.WriteLine(car.BrandName + " " +car.Description);
             }
 
         }
