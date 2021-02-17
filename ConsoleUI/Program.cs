90aaf7ac3397c41450eca7b9d4441e94f6fa38a6
MehmetUgur7 <77232030+MehmetUgur7@users.noreply.github.com>
2021-02-17 11:40:13 +0300

    6 Subat 2.mola

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,12 +9,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
+
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+
+        }
+
+        private static void CarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Descriptions);
             }
-
         }
     }
 }
